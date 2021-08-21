465bce386527c5d1f4547886011679dfbad56b0e
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-08-21 19:46:25 -0300

    Primeiro commit!

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,30 @@
+import { Typography } from "@material-ui/core";
+import React, { useState } from "react";
+import DadosEntrega from "./DadosEntrega";
+import DadosPessoais from "./DadosPessoais";
+import DadosUsuario from "./DadosUsuario";
+
+function FormularioCadastro({ aoEnviar, validarCPF }) {
+  const [etapaAtual, setEtapaAtual] = useState(0);
+
+  function formularioAtual(etapa) {
+    switch (etapa) {
+      case 0:
+        return <DadosUsuario />;
+      case 1:
+        return <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>;
+      case 2:
+        return <DadosEntrega />;
+      default:
+        return <Typography>Erro</Typography>
+  
+    }
+  }
+  return (
+    <>
+      {formularioAtual(etapaAtual)}
+    </>
+  );
+}
+
+export default FormularioCadastro;
