668c3863ff90984f992a8ee74cf30c001a7ba9e5
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-08-22 17:10:38 -0300

    Validação adicionada ao contexto da aplicação utilizando useContext.

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import "./App.css";
 import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
 import 'fontsource-roboto';
 
-import {Container, Typography } from "@material-ui/core"
+import { Container, Typography } from "@material-ui/core"
 import { validarCPF, validarSenha, validarNome } from "./models/cadastro";
+import ValidacoesCadastro from "./contexts/ValidacoesCadastro";
 class App extends Component {
   render() {
     return (
       <Container component="article" maxWidth="sm">
         <Typography variant="h3" component="h1" align="center" >Formulário de cadastro</Typography>
-        <FormularioCadastro aoEnviar={aoEnviarForm} validacoes={{cpf:validarCPF, senha:validarSenha, nome:validarNome }} />
+        <ValidacoesCadastro.Provider value={{ cpf: validarCPF, senha: validarSenha, nome: validarNome }}>
+          <FormularioCadastro aoEnviar={aoEnviarForm} />
+        </ValidacoesCadastro.Provider>
       </Container>
     );
   }
 }
 
-function aoEnviarForm(dados){
+function aoEnviarForm(dados) {
   console.log(dados);
 }
 
